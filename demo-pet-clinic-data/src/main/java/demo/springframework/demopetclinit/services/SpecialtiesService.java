1b522fc9565165025894b0ad3d85edf0a8bdc281
rafanegrette <rafanegrette@hotmail.com>
2019-02-10 17:51:05 -0500

    Added Specialties Map Imp. Closes #31.

---
+++ demo-pet-clinic-data/src/main/java/demo/springframework/demopetclinit/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package demo.springframework.demopetclinit.services;
+
+import demo.springframework.demopetclinit.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
