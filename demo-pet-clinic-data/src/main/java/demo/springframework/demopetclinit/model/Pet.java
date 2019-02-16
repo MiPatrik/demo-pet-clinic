9816662efad4b19cc128c502f4e19373c41a53d6
rafanegrette <rafanegrette@hotmail.com>
2019-02-16 12:47:25 -0500

    Added visit entity. Closes #38.

--- demo-pet-clinic-data/src/main/java/demo/springframework/demopetclinit/model/Pet.java
+++ demo-pet-clinic-data/src/main/java/demo/springframework/demopetclinit/model/Pet.java
@@ -1,9 +1,9 @@
 package demo.springframework.demopetclinit.model;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -16,7 +16,11 @@ public class Pet extends BaseEntity {
 
     @ManyToOne
     private Owner owner;
+
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits =new HashSet<>();
 
     public String getName() {
         return name;
@@ -48,5 +52,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
