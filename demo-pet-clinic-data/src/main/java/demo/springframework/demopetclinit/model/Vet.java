8d5933af9e14bace3d7dbfd59d461516108dbfb0
rafanegrette <rafanegrette@hotmail.com>
2019-02-19 11:50:59 -0500

    Added project lombok and refactor . closes #50

--- demo-pet-clinic-data/src/main/java/demo/springframework/demopetclinit/model/Vet.java
+++ demo-pet-clinic-data/src/main/java/demo/springframework/demopetclinit/model/Vet.java
@@ -1,9 +1,17 @@
 package demo.springframework.demopetclinit.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -13,11 +21,4 @@ public class Vet extends Person{
             inverseJoinColumns=@JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
-    public Set<Specialty> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
-    }
 }
