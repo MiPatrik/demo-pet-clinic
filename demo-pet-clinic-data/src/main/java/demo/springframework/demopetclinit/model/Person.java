95de81b0b857acf2e86d756b8c42dd18e450ab99
rafanegrette <rafanegrette@hotmail.com>
2019-02-27 16:28:45 -0500

    Added test for Owner Map Service . closes #48

--- demo-pet-clinic-data/src/main/java/demo/springframework/demopetclinit/model/Person.java
+++ demo-pet-clinic-data/src/main/java/demo/springframework/demopetclinit/model/Person.java
@@ -1,16 +1,26 @@
 package demo.springframework.demopetclinit.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.MappedSuperclass;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
 
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
